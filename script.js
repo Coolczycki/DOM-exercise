044d11a14747ccf7a9e8f77ecc36447f97a0f4da
Piotr_Kulczycki <Piotr_kulczycki@icloud.com>
2023-01-09 14:38:07 +0100

    Javascript instructions and src link

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+ DOM exercise; 
+01. <p> with red text that says “Hey I’m red!”
+02. <h3> with blue text that says “I’m a blue h3!”
+03. <div> with a black border and pink background color with the following elements inside of it:
+        03 A) another <h1> that says “I’m in a div”
+        03 B) a <p> that says “ME TOO!”
+                Hint for this one: after creating the <div> with createElement, 
+        03 C) append the <h1> and <p> to it before adding it to the container
+*/
+
