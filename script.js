3ce3f344b4b146575c4a706c4b9b007fc4e1b952
Piotr_Kulczycki <Piotr_kulczycki@icloud.com>
2023-01-11 11:24:24 +0100

    Exercise 03

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
  DOM exercise; 
 01. <p> with red text that says “Hey I’m red!”
 02. <h3> with blue text that says “I’m a blue h3!”
-03. <div> with a black border and pink background color with the following elements inside of it:
+03. <div> with a black border and pink background
+ color with the following elements inside of it:
+
         03 A) another <h1> that says “I’m in a div”
         03 B) a <p> that says “ME TOO!”
                 Hint for this one: after creating the <div> with createElement, 
@@ -23,3 +25,13 @@ const hT = document.createElement('h3');
 hT.textContent = "I\'m a blue h3!"; 
 hT.style.color = 'Blue';
 hThree.appendChild(hT);
+
+// 03: div container
+
+const diver = document.querySelector('#container');
+const diverCont = document.createElement('div');
+diverCont.style.width = '100px';
+diverCont.style.height = '100px';
+diverCont.style.border = 'thin solid black';
+diverCont.style.backgroundColor = 'Pink';
+diver.appendChild(diverCont);
